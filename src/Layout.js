c774a082d54ee3fb314b76c6cf52ddb9b2ef11ad
Donna Mesker <donnamesker1@gmail.com>
2025-01-28 14:05:30 -0500

    currency converter

--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,7 @@ const Layout = (props) => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">Currency Exchange Rates</span>
+        <span className="navbar-brand mb-0 ms-2 h1">Currency Exchange Rates</span>
       </nav>
       <div className="container py-3">
         {props.children}
@@ -14,7 +14,7 @@ const Layout = (props) => {
           <a className="badge badge-dark" href="https://github.com/Altcademy/exchange-rate-site" target="_blank">GitHub</a>
         </div>
         <div>
-          <span className="mr-3 text-secondary">Built by <a href="https://www.altcademy.com" target="_blank">Altcademy</a> with ☕ and 💜</span>
+          <span className="me-3 text-secondary">Built by <a href="https://www.altcademy.com" target="_blank">Altcademy</a> with ☕ and 💜</span>
         </div>
       </footer>
     </React.Fragment>
